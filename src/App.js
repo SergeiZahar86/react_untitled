754e4936acb7a799a9b8b56293248f667d2bfae8
Сергей Захаров <sergei86zahar@mail.ru>
2021-03-03 18:05:49 +0700

    1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import TodoList from "./ToDo/TodoList";
 import Context from "./context";
-import AddTodo from "./ToDo/AddTodo";
+import Loader from "./Loader";
+import Modal from "./Modal/Modal";
+//import AddTodo from "./ToDo/AddTodo";
+
+
+// useEffect - новый хук в реакт
+
+// ленивая загрузка
+const AddTodo = React.lazy(() => import('./ToDo/AddTodo'))
+
+
 
 function App()
 {
 	const [todos, setTodos] = React.useState([
-		{id: 1, completed: false, title: 'Купить хлеб'},
+		/*{id: 1, completed: false, title: 'Купить хлеб'},
 		{id: 2, completed: true, title: 'Купить масло'},
-		{id: 3, completed: false, title: 'Купить молоко'}
+		{id: 3, completed: false, title: 'Купить молоко'}*/
 	]);
+	const [loading, setLoading] = React.useState(true)
 
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/todos?_limit=5').then(respons => respons.json()).
+		then(todos => {
+			setTimeout(() => {
+				setTodos(todos)
+				setLoading(false)
+			}, 2000)
+
+		})
+	}, [])
 
 	function toggleTodo(id)
 	{
@@ -49,8 +70,16 @@ function App()
 		<Context.Provider value={{removeTodo}}>
 			<div className={'wrapper'}>
 				<h1>React tutorial</h1>
-				<AddTodo onCreate={addTodo}/>
-				{todos.length ? <TodoList todosss={todos} onToggle={toggleTodo}/> : <p>No todos!</p>}
+				<Modal/>
+
+				// ленивая загрузка
+				<React.Suspense fallback={<p>Loading....</p>}>
+					<AddTodo onCreate={addTodo}/>
+				</React.Suspense>
+
+				{/*<AddTodo onCreate={addTodo}/>*/}
+				{loading && <Loader/>}
+				{todos.length ? <TodoList todosss={todos} onToggle={toggleTodo}/> : (loading ? null : <p>No todos!</p>)}
 			</div>
 		</Context.Provider>)
 }
---
+++ src/Loader.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// https://loading.io/css/
+export default () => <div style={{
+	display: 'flex',
+	justifyContent: 'center',
+	margin: '.5rem'
+}}>
+		<div className="lds-dual-ring"></div>
+	</div>
